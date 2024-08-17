16448fa89368ef70e5c211dcef39c6696d9ce340
Gaurav Sablok <codeprog@icloud.com>
2024-08-17 09:43:01 +0200

    codecreatede

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # readvariantannot.jl
-a read variant annotator written in julia
+
+- implementing mutable structs for genes and pangenes. 
+- uses Int64 so that no bufferflow @btime: a samfile with 5GB parsed in less than 10 minutes. 
+- a kmerplot function added, which is available as a functional closure julia programming and calls a kmer to the reads which harbours the variants.
+- to add: a gene functional annotation link so that the variants can be linked to the gene and also annotated. will implement a closure. 
+
+```
+include readvariantannot.jl
+
+function samReadsAll(samplesame, samplevcf)
+# will prepare all and will return the reads spanning 
+(418, 952, "CCAGGCAGTGGCAGGTGGCCACCGTCCTCTCTGCCCCCGCCAAAATCACCAACCATCTGGTAGCGATGAT")
+(210, 743, "CATTACCACCACCATCACCATTACCACAGGAAACGGTGCGGGCTGACGCGTACAGGAAACACCGAAAAAA")
+(239, 759, "AACGCGGCGCTGATTTGCCGTGGCGAGAAAATGTCGATCGCCATTAGGGCCGGCCTGTTAGAAGCGCGTG")
+(334, 899, "CCATGCTAGTGTTGAAGTTCTGCGGTACATCAGTGGCAAATGCAGAACGTTTTCTGCGGGTTGCCGATAT")
+(330, 843, "GTTATCGATCCGGTCGTAAAACTGCTGGCAGTGGGTCATTACCTCGAATCTACCGTTGATATTGATGAAT")
+
+function samReadsUser(samplesame, samplevcf, variantdefined)
+# will prepare for that position specific and will return the reads spanning 
+(418, 952, "CCAGGCAGTGGCAGGTGGCCACCGTCCTCTCTGCCCCCGCCAAAATCACCAACCATCTGGTAGCGATGAT")
+(210, 743, "CATTACCACCACCATCACCATTACCACAGGAAACGGTGCGGGCTGACGCGTACAGGAAACACCGAAAAAA")
+(239, 759, "AACGCGGCGCTGATTTGCCGTGGCGAGAAAATGTCGATCGCCATTAGGGCCGGCCTGTTAGAAGCGCGTG")
+(334, 899, "CCATGCTAGTGTTGAAGTTCTGCGGTACATCAGTGGCAAATGCAGAACGTTTTCTGCGGGTTGCCGATAT")
+(330, 843, "GTTATCGATCCGGTCGTAAAACTGCTGGCAGTGGGTCATTACCTCGAATCTACCGTTGATATTGATGAAT")
+```
+
+Gaurav Sablok \
+University of Potsdam \
+Potsdam,Germany
+
---
+++ readvariantannot.jl
@@ -0,0 +1,119 @@
+#! /usr/bin/env julia
+# Author Gaurav Sablok
+# University of Potsdam
+# Potsdam,Germany
+
+# Date 2024-7-3
+
+function samReadUser(samfile, variantfile, variantdefined)
+    # reading the sam for the variants and indexing the variants as Any[] and then mapping the reads to variants by the position
+    # no memory loading needed making the annotations faster.
+    # using Int64 so that no buffer overflow occurs.
+    readmapStart = Any[]
+    readmapEnd = Any[]
+    readMIDTags = Any[]
+    readString = String[]
+    readfile = readlines(open(samfile))
+    for i in 1:length(readfile)
+        if !startswith(readfile[i], "@")
+            push!(readmapStart,split(split(readfile[i], r"\t")[1], r"_")[3])
+            push!(readmapEnd, split(split(readfile[i], r"\t")[1], r"_")[4])
+            push!(readMIDTags,split(readfile[i], r"\t")[6])
+            push!(readString, split(readfile[i], r"\t")[10])
+        end
+    end
+    varaintPos = Any[]
+    readvariants = readlines(open(variantfile))
+    for i in 1:readvariants
+        if !startswith(readvariants[i], r'#|##')
+          push!(variantPos, split(readvariants[i], "\t")[2])
+        end
+    end
+   variantdefined = Any[]
+   iterstart = [(parse(Int64,readmapStart[i]),parse(Int64,readmapEnd[i]), readString[i]) for i in 1:length(readmapStart)]
+   getReads = Any[]
+    for i in 1:length(iterstart)
+        for j in 1:length(variantdefined)
+            if iterstart[i][1] < variantdefined[j] && iterstart[i][2] > variantPos[j]
+                push!(getReads, iterStart[i])
+            end
+        end
+    end
+end
+
+function samReadsAll(samfile, variantfile)
+    readmapStart = Any[]
+    readmapEnd = Any[]
+    readMIDTags = Any[]
+    readString = String[]
+    readfile = readlines(open(samfile))
+    for i in 1:length(readfile)
+        if !startswith(readfile[i], "@")
+            push!(readmapStart,split(split(readfile[i], r"\t")[1], r"_")[3])
+            push!(readmapEnd, split(split(readfile[i], r"\t")[1], r"_")[4])
+            push!(readMIDTags,split(readfile[i], r"\t")[6])
+            push!(readString, split(readfile[i], r"\t")[10])
+        end
+    end
+    varaintPos = Any[]
+    readvariants = readlines(open(variantfile))
+    for i in 1:readvariants
+        if !startswith(readvariants[i], r'#|##')
+          push!(variantPos, split(readvariants[i], "\t")[2])
+        end
+    end
+   iterstart = [(parse(Int64,readmapStart[i]),parse(Int64,readmapEnd[i]), readString[i]) for i in 1:length(readmapStart)]
+   getReads = Any[]
+    for i in 1:length(iterstart)
+        for j in 1:length(variantPos)
+            if iterstart[i][1] < variantPos[j] && iterstart[i][2] > variantPos[j]
+                push!(getReads, iterStart[i])
+            end
+        end
+    end
+
+
+function samReadsKmerPlot(samfile, variantfile)
+    readmapStart = Any[]
+    readmapEnd = Any[]
+    readMIDTags = Any[]
+    readString = String[]
+    readfile = readlines(open(samfile))
+    for i in 1:length(readfile)
+        if !startswith(readfile[i], "@")
+            push!(readmapStart,split(split(readfile[i], r"\t")[1], r"_")[3])
+            push!(readmapEnd, split(split(readfile[i], r"\t")[1], r"_")[4])
+            push!(readMIDTags,split(readfile[i], r"\t")[6])
+            push!(readString, split(readfile[i], r"\t")[10])
+        end
+    end
+    varaintPos = Any[]
+    readvariants = readlines(open(variantfile))
+    for i in 1:readvariants
+        if !startswith(readvariants[i], r'#|##')
+          push!(variantPos, split(readvariants[i], "\t")[2])
+        end
+    end
+   iterstart = [(parse(Int64,readmapStart[i]),parse(Int64,readmapEnd[i]), readString[i]) for i in 1:length(readmapStart)]
+   getReads = Any[]
+    for i in 1:length(iterstart)
+        for j in 1:length(variantPos)
+            if iterstart[i][1] < variantPos[j] && iterstart[i][2] > variantPos[j]
+                push!(getReads, iterStart[i])
+            end
+        end
+    end
+    function kmerplot(frequency)
+        ### adding a closure here for the kmer so that it can be called for any stored variable with in the function.
+        ### this can tell you the type of the kmers associated with the variants. 
+        selreadString = [getReads[i][3] i in 1:length(getReads)]
+        selreadsStart = [getReads[i][1] i in 1:length(getReads)]
+        selreadEnd = [getReads[i][2] i in 1:length(getReads)]
+        storingsKmer = Any[]
+        @asset length(selreadsStart) == length(selreadEnd) == length(selreadString)
+        for i in 1:length(selreadstring)-parse(Int64,frequency)
+            push!(storingsKmer, [selreadStart[i], selreadEnd[i], selreadString[i:i+parse(Int64, frequency)]
+        end
+    end
+end
+
